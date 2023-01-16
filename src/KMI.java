6c6ae581e64963b61fb950c7ad04f5eb73da66e1
zidau <zi.daugela@gmail.com>
2023-01-17 01:51:49 +0200

    Atnaujinta

--- src/KMI.java
+++ src/KMI.java
@@ -3,7 +3,7 @@ public class KMI {
     public static void main(String[] args) {
         float KMIsvoris, KMIugis, kmi;
         Scanner skaneris = new Scanner(System.in);
-        System.out.println("Kūno masės indekso skaičiuoklė ");
+        System.out.println("Kūno masės indekso skaičiuoklė!");
 
         System.out.print("Įveskite savo svorį: ");
         KMIsvoris = skaneris.nextFloat();
@@ -13,6 +13,8 @@ public class KMI {
 
         kmi = KMIsvoris / (KMIugis * KMIugis);
         System.out.println("Jūsų kuno masės indeksas yra: " +kmi);
+
+        System.out.println("Tik testuoju");
 
     }
 }
