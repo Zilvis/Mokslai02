1d880eac6038ba9a50d8efd514a09b4008f344ef
zidau <zi.daugela@gmail.com>
2023-01-17 01:43:01 +0200

    Update

---
+++ src/KMI.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class KMI {
+    public static void main(String[] args) {
+        float KMIsvoris, KMIugis, kmi;
+        Scanner skaneris = new Scanner(System.in);
+        System.out.println("Kūno masės indekso skaičiuoklė");
+
+        System.out.print("Įveskite savo svorį: ");
+        KMIsvoris = skaneris.nextFloat();
+
+        System.out.print("Įveskite savo ūgį (PVZ 1,85): ");
+        KMIugis = skaneris.nextFloat();
+
+        kmi = KMIsvoris / (KMIugis * KMIugis);
+        System.out.println("Jūsų kuno masės indeksas yra: " +kmi);
+
+    }
+}
