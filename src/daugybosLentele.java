20a508e06531e256285367c3aa212c89b009f09c
zidau <zi.daugela@gmail.com>
2023-01-17 15:25:06 +0200

    Daugybos lentele is 10

---
+++ src/daugybosLentele.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class daugybosLentele {
+    public static void main(String[] args) {
+        Scanner skaneris = new Scanner(System.in);
+        float pirmasSkaicius;
+        float antrasSkaicius;
+        float atsakymas;
+
+        System.out.print("Įveskite pirmajį skaičių: ");
+        pirmasSkaicius = skaneris.nextFloat();
+        System.out.print("Įveskite antrajį skaičių");
+        antrasSkaicius = skaneris.nextFloat();
+        atsakymas = (pirmasSkaicius * antrasSkaicius);
+
+        System.out.println(pirmasSkaicius);
+        System.out.println(antrasSkaicius);
+
+        System.out.println(pirmasSkaicius+ " * " +antrasSkaicius+ " = " +atsakymas);
+
+
+    }
+}
