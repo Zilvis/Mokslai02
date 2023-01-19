1eba586b84afdaf881cb031483faee9982b49908
ZilvisMainPC <zi.daugela@gmail.com>
2023-01-19 20:22:37 +0200

    Kaip ir pabaigtas

--- src/mokslai/ifelse.java
+++ src/mokslai/ifelse.java
@@ -3,17 +3,32 @@ import java.util.Scanner;
 public class ifelse {
     public static void main(String[] args) {
         int sk = 9;
+        String simbolis;
+
         Scanner skaneris = new Scanner(System.in);
         System.out.println("Įveskite skaičių: ");
         sk = skaneris.nextInt();
+
+        Scanner skaneris2 = new Scanner(System.in);
+        System.out.println("Įveskite simbolį: ");
+        simbolis = skaneris2.nextLine();
+        //System.out.println(simbolis); //Debug
+
         for (int i = 1; i < 10; i++) {
-            int atsakymas = i * sk;
-            if (atsakymas < 50) {
-                System.out.println(sk + "*" + i + " = " + atsakymas+ " <50");
-            }else if (atsakymas == 50) {
-                System.out.println(sk + "*" + i + " = " + atsakymas+" == 50");
-            }else {
-                System.out.println(sk + "*" + i + " = " + atsakymas+ " > 50");
+            int atsakymas = 0;
+            atsakymas = i * sk;
+            if (simbolis.equals("/")) { //eilutes lyginti tik per funkcija equals
+                System.out.println("Atliksime dalybos veiksmus:");
+            } else if (simbolis.equals("*") && atsakymas < 50) {
+                System.out.println(sk + "*" + i + " = " +atsakymas+ " <50");
+            }else if (simbolis.equals("*") && atsakymas == 50) {
+                System.out.println(sk + "*" + i + " = " +atsakymas+" == 50");
+            }else if (simbolis.equals("*") && atsakymas > 50) {
+                System.out.println(sk + "*" + i + " = " +atsakymas+ " > 50");
+            }
+            else { //Visais kitais atvejais
+                System.out.println("Neteisingas simbolis");
+                break; // sutabdo koda kad viena karta atvaizduotu !
             }
         }
     }
