68031d2d03816d9464f5007efdfefe12ae62ce57
ZilvisMainPC <zi.daugela@gmail.com>
2023-01-19 21:36:27 +0200

    Kaip ir pabaigtas

--- src/mokslai/ifelse.java
+++ src/mokslai/ifelse.java
@@ -1,43 +1,44 @@
 package mokslai;
+
 import java.util.Scanner;
+
 public class ifelse {
     public static void main(String[] args) {
-        int sk = 9;
+        float sk = 9;
         String simbolis;
 
         Scanner skaneris = new Scanner(System.in);
         System.out.println("Įveskite skaičių: ");
-        sk = skaneris.nextInt();
+        sk = skaneris.nextFloat();
 
         Scanner skaneris2 = new Scanner(System.in);
         System.out.println("Įveskite simbolį: ");
         simbolis = skaneris2.nextLine();
         //System.out.println(simbolis); //Debug
 
-        for (int i = 1; i < 10; i++) {
-            int atsakymas = 0;
-            atsakymas = i * sk;
+        for (float i = 1; i < 10; i++) {
+            float daugyba;
+            float dalyba;
+
             if (simbolis.equals("/")) { //eilutes lyginti tik per funkcija equals
-                System.out.println("Atliksime dalybos veiksmus:");
-                if (simbolis.equals("/") && atsakymas < 50) {
-                    System.out.println(sk + " / " + i + " = " +atsakymas+ " <50");
-                }else if (simbolis.equals("/") && atsakymas == 50) {
-                    System.out.println(sk + " / " + i + " = " +atsakymas+" == 50");
-                }else if (simbolis.equals("/") && atsakymas > 50) {
-                    System.out.println(sk + " / " + i + " = " +atsakymas+ " > 50");
+                dalyba = (sk / i);
+                if (dalyba < 50) {
+                    System.out.println(sk + " / " + i + " = " + dalyba + " <50");
+                } else if (dalyba == 50) {
+                    System.out.println(sk + " / " + i + " = " + dalyba + " == 50");
+                } else if (dalyba > 50) {
+                    System.out.println(sk + " / " + i + " = " + dalyba + " > 50");
                 }
-                System.out.println("Atliksime daugybos veiksmus:");
-            } else if (simbolis.equals("*") && atsakymas < 50) {
-                System.out.println(sk + " * " + i + " = " +atsakymas+ " <50");
-            }else if (simbolis.equals("*") && atsakymas == 50) {
-                System.out.println(sk + " * " + i + " = " +atsakymas+" == 50");
-            }else if (simbolis.equals("*") && atsakymas > 50) {
-                System.out.println(sk + " * " + i + " = " +atsakymas+ " > 50");
-            }
-            else { //Visais kitais atvejais
-                System.out.println("Neteisingas simbolis");
-                break; // sutabdo koda kad viena karta atvaizduotu !
-            }
-        }
-    }
-}
+            } else {
+                daugyba = (i * sk);
+                if (daugyba < 50) {
+                    System.out.println(sk + " * " + i + " = " + daugyba + " <50");
+                } else if (daugyba == 50) {
+                    System.out.println(sk + " * " + i + " = " + daugyba + " == 50");
+                } else if (daugyba > 50) {
+                    System.out.println(sk + " * " + i + " = " + daugyba + " > 50");
+                }
+        } // salygos su simbolio nustatymo pabaiga
+    } //for pabaiga
+} // funkcijos main end
+} //baigesi klase
